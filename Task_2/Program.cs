b0376847374c0220ad534ffaef6aa6d6f1441946
Pavel Ordenov <pavelordenov@yandex.ru>
2023-05-22 20:36:01 +0200

    Finished Task2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -2,4 +2,13 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-Console.WriteLine("HI");
+
+Console.Write("Введите первое чилсло, ");
+double num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе чилсло, ");
+double num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) Console.WriteLine("Наибольшее число из двух - " + num1);
+else if (num2 > num1) Console.WriteLine("Наибольшее число из двух - " + num2);
+else Console.WriteLine("Числа равны");
